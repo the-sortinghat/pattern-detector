44526ce6cf8e10af172671d30502c26a675d6ffd
João Daniel <jotaf.daniel@gmail.com>
2021-10-29 05:19:05 -0300

    add services to systems
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- src/domain/System.spec.ts
+++ src/domain/System.spec.ts
@@ -1,13 +1,14 @@
+import { Service } from './Service'
 import { System } from './System'
 
 describe(System, () => {
+  let system: System
+
+  beforeEach(() => {
+    system = System.create('foo')
+  })
+
   describe('create', () => {
-    let system: System
-
-    beforeEach(() => {
-      system = System.create('foo')
-    })
-
     it('returns a System instance', () => {
       expect(system).toBeInstanceOf(System)
     })
@@ -18,6 +19,19 @@ describe(System, () => {
 
     it('returns a System with ID', () => {
       expect(system.id).toBeDefined()
+    })
+  })
+
+  describe('addService', () => {
+    let service: Service
+
+    beforeEach(() => {
+      service = Service.create('foo')
+      system.addService(service)
+    })
+
+    it('add the service to the system', () => {
+      expect(system.services).toContain(service)
     })
   })
 })
--- src/domain/System.ts
+++ src/domain/System.ts
@@ -1,9 +1,22 @@
 import { randomUUID as uuid } from 'crypto'
+import { Service } from './Service'
 
 export class System {
   static create(name: string): System {
     return new System(name)
   }
 
-  private constructor(public readonly name: string, public readonly id = uuid()) {}
+  private _services: Service[]
+
+  private constructor(public readonly name: string, public readonly id = uuid()) {
+    this._services = []
+  }
+
+  public addService(service: Service): void {
+    this._services.push(service)
+  }
+
+  public get services(): Service[] {
+    return Object.assign([], this._services)
+  }
 }
