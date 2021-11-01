68560438eec727b6a36926823451e93b77d7dd9a
João Daniel <jotaf.daniel@gmail.com>
2021-11-01 18:36:12 -0300

    add System Repository interface
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ src/domain/utils/SystemRepository.interface.ts
@@ -0,0 +1,6 @@
+import { System } from '../model/System'
+
+export interface ISystemRepository {
+  save: (system: System) => Promise<System>
+  update: (sID: string, updated: System) => Promise<System>
+}
