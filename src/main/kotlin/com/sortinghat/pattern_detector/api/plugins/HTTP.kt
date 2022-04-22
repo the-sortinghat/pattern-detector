7ab8c1be286aef7e2e3a4556e4728afb297df9d9
João Daniel <jotaf.daniel@gmail.com>
2022-04-22 17:51:20 -0300

    add ktor plugins for JSON and CORS

--- src/main/kotlin/com/sortinghat/pattern_detector/api/plugins/HTTP.kt
+++ src/main/kotlin/com/sortinghat/pattern_detector/api/plugins/HTTP.kt
@@ -1,6 +1,18 @@
 package com.sortinghat.pattern_detector.api.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.*
 
 @Suppress("unused")
-fun Application.configureHTTP() {}
+fun Application.configureHTTP() {
+    install(CORS) {
+        anyHost()
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Delete)
+        allowHeader(HttpHeaders.ContentType)
+        allowHeader(HttpHeaders.Authorization)
+    }
+}
--- src/main/kotlin/com/sortinghat/pattern_detector/api/plugins/Serialization.kt
+++ src/main/kotlin/com/sortinghat/pattern_detector/api/plugins/Serialization.kt
@@ -1,6 +1,12 @@
 package com.sortinghat.pattern_detector.api.plugins
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
 
 @Suppress("unused")
-fun Application.configureSerialization() {}
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+}
