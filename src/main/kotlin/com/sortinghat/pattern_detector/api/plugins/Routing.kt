812a38c6ae6ead83c27ed5a9b3b9da9fd7ab1517
João Daniel <jotaf.daniel@gmail.com>
2022-04-23 21:28:52 -0300

    implement route to get all patterns in a system

--- src/main/kotlin/com/sortinghat/pattern_detector/api/plugins/Routing.kt
+++ src/main/kotlin/com/sortinghat/pattern_detector/api/plugins/Routing.kt
@@ -1,16 +1,37 @@
 package com.sortinghat.pattern_detector.api.plugins
 
+import com.sortinghat.pattern_detector.domain.model.ServiceRepository
+import com.sortinghat.pattern_detector.domain.services.DatabasePerServiceDetector
+import com.sortinghat.pattern_detector.domain.services.MetricCollector
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Application.configureRouting() {
+fun Application.configureRouting(serviceRepository: ServiceRepository) {
     routing {
-        get("/") {
+        get("/systems/{slug}/patterns") {
+            val slugParam = call.parameters["slug"]
+                ?: call.respond(
+                    status = HttpStatusCode.BadRequest,
+                    message = mapOf("error" to "slug must be provided")
+                )
+
+            val services = serviceRepository.findAllOfSystem(slugParam as String)
+
+            val metricCollector = MetricCollector()
+            val databasePerServiceDetector = DatabasePerServiceDetector()
+
+            services.forEach { it.accept(metricCollector) }
+            services.forEach { it.accept(databasePerServiceDetector) }
+
+            val dbps = databasePerServiceDetector.getResults()
+
+            val patterns = mapOf("DatabasePerService" to dbps)
+
             call.respond(
                 status = HttpStatusCode.OK,
-                message = "Hello, Ktor!"
+                message = mapOf("patterns" to patterns)
             )
         }
     }
