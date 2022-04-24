eaac94622faedac70e1e9e1c3d47ab900f778775
João Daniel <jotaf.daniel@gmail.com>
2022-04-24 15:25:14 -0300

    pass instance of service repository to routing

--- src/main/kotlin/com/sortinghat/pattern_detector/Application.kt
+++ src/main/kotlin/com/sortinghat/pattern_detector/Application.kt
@@ -3,6 +3,7 @@ package com.sortinghat.pattern_detector
 import com.sortinghat.pattern_detector.api.plugins.configureHTTP
 import com.sortinghat.pattern_detector.api.plugins.configureRouting
 import com.sortinghat.pattern_detector.api.plugins.configureSerialization
+import com.sortinghat.pattern_detector.db.ServiceRepositoryImpl
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
 
@@ -10,7 +11,9 @@ fun main(args: Array<String>) = EngineMain.main(args)
 
 @Suppress("unused")
 fun Application.module() {
+    val serviceRepository = ServiceRepositoryImpl()
+
     configureSerialization()
     configureHTTP()
-    configureRouting()
+    configureRouting(serviceRepository)
 }
