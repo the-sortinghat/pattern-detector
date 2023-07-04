12feb863306941623ec1941f1ed0ce05b9dc2fed
marcelo-vidu <marcelovidu@gmail.com>
2023-07-04 00:19:43 -0300

    Fix smell output duplicates and multiple instance detection.

--- src/main/kotlin/com/sortinghat/pattern_detector/domain/services/CyclicDependenciesSmellDetector.kt
+++ src/main/kotlin/com/sortinghat/pattern_detector/domain/services/CyclicDependenciesSmellDetector.kt
@@ -9,8 +9,7 @@ import com.sortinghat.pattern_detector.domain.model.smells.CyclicDependenciesSme
 class CyclicDependenciesSmellDetector(private val inputServices: List<Service>) : Visitor, SmellDetector {
 
     private val visited = mutableSetOf<Visitable>()
-    private val cyclicDependencies = mutableMapOf<Service, MutableList<Service>>()
-    private val cyclicDependenciesDetected = mutableSetOf<Service>()
+    private val cyclicDependencies = mutableSetOf<CyclicDependenciesSmell>()
     private val visitedServices = mutableSetOf<Service>()
 
     override fun visit(dependencies: ServiceDependency) {
@@ -24,39 +23,32 @@ class CyclicDependenciesSmellDetector(private val inputServices: List<Service>) 
         if (service in visited || service in visitedServices) return
 
         val path = mutableListOf<Service>()
-        val cyclicDependencyPath = detectCyclicDependencies(service, path)
-
-        if (cyclicDependencyPath.isNotEmpty() && service !in cyclicDependenciesDetected) {
-            cyclicDependencies[service] = cyclicDependencyPath.toMutableList()
-            cyclicDependenciesDetected.add(service)
-        }
+        detectCyclicDependencies(service, path)
 
         visited.add(service)
         visitedServices.add(service)
         service.children().forEach { it.accept(visitor = this) }
     }
 
-    private fun detectCyclicDependencies(service: Service, path: MutableList<Service>): List<Service> {
+    private fun detectCyclicDependencies(service: Service, path: MutableList<Service>) {
         if (service in path) {
             // Cyclic dependency detected
-            return path.subList(path.indexOf(service), path.size)
+            val cyclicDependencyPath = path.subList(path.indexOf(service), path.size)
+            val cyclicDependenciesSmell = CyclicDependenciesSmell.from(service, cyclicDependencyPath)
+            cyclicDependencies.add(cyclicDependenciesSmell)
+            return
         }
 
         path.add(service)
 
-        var cyclicDependencyPath: List<Service> = emptyList()
-
         service.dependencies.forEach { dependency ->
             val dependencyService = getServiceFromDependency(dependency)
             if (dependencyService != null && !visitedServices.contains(dependencyService)) {
-                cyclicDependencyPath = detectCyclicDependencies(dependencyService, path.toMutableList())
-                if (cyclicDependencyPath.isNotEmpty()) {
-                    return cyclicDependencyPath
-                }
+                detectCyclicDependencies(dependencyService, path.toMutableList())
             }
         }
 
-        return cyclicDependencyPath
+        path.remove(service)
     }
 
     private fun getServiceFromDependency(dependency: ServiceDependency): Service? {
@@ -99,8 +91,6 @@ class CyclicDependenciesSmellDetector(private val inputServices: List<Service>) 
     }
 
     override fun getResults(): Set<CyclicDependenciesSmell> {
-        return cyclicDependencies.map { (service, dependencies) ->
-            CyclicDependenciesSmell.from(service, dependencies)
-        }.toSet()
+        return cyclicDependencies.toSet()
     }
 }
