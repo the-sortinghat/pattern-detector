c401eb3f236340a9a33dcfbc69a56a742017b85c
marcelo-vidu <marcelovidu@gmail.com>
2024-06-16 23:24:43 -0300

    Add serialization where it was not present to fix some compiler errors.

--- app-analyses/src/main/kotlin/com/usvision/analyses/detector/AsyncMessagingInstance.kt
+++ app-analyses/src/main/kotlin/com/usvision/analyses/detector/AsyncMessagingInstance.kt
@@ -1,7 +1,9 @@
 package com.usvision.analyses.detector
 
 import com.usvision.model.domain.Microservice
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class AsyncMessagingInstance(
     val publisher: Microservice,
     val subscriber: Microservice
--- app-analyses/src/main/kotlin/com/usvision/analyses/detector/CyclicDependencyInstance.kt
+++ app-analyses/src/main/kotlin/com/usvision/analyses/detector/CyclicDependencyInstance.kt
@@ -1,7 +1,9 @@
 package com.usvision.analyses.detector
 
 import com.usvision.model.domain.Microservice
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class CyclicDependencyInstance(
     val members: Set<Microservice>
 ) : ArchitectureInsight {
@@ -14,9 +16,18 @@ data class CyclicDependencyInstance(
             return CyclicDependencyInstance(members)
         }
 
-        private fun parseParents(currentSet: Set<Microservice>, child: Microservice, parentOf: Map<Microservice, Microservice>): Set<Microservice> {
+        private fun parseParents(
+            currentSet: Set<Microservice>,
+            child: Microservice,
+            parentOf: Map<Microservice, Microservice>
+        ): Set<Microservice> {
             if (child in currentSet) return currentSet
-            return parseParents(currentSet + child, parentOf[child]!!, parentOf)
+            val parent = parentOf[child]
+            return if (parent != null) {
+                parseParents(currentSet + child, parent, parentOf)
+            } else {
+                currentSet
+            }
         }
     }
 }
