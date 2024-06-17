f4bb1e98ca3fd7c11cb779249bd0c66d3284250b
marcelo-vidu <marcelovidu@gmail.com>
2024-06-17 20:07:22 -0300

    Change NumberOfDependencies class and NumberOfDependenciesTest to consider the number of publishers in a queue subscribed by ms instead of the counting only the number of subscriptions. Add some more scenarios of test.

--- app-analyses/src/main/kotlin/com/usvision/analyses/analyzer/NumberOfDependencies.kt
+++ app-analyses/src/main/kotlin/com/usvision/analyses/analyzer/NumberOfDependencies.kt
@@ -1,24 +1,85 @@
 package com.usvision.analyses.analyzer
 
 import com.usvision.model.domain.Microservice
+import com.usvision.model.domain.databases.Database
 import com.usvision.model.visitor.Visitable
+import com.usvision.model.domain.MessageChannel
+import com.usvision.model.domain.operations.Operation
 
 class NumberOfDependencies : Measurer() {
     private val INT_TYPE_NAME = Int::class.qualifiedName!!
     private val UNIT_DEPENDENCIES = "dependencies"
 
-    private val counters: MutableMap<Visitable, Measure> = mutableMapOf()
+    private val publishers: MutableMap<MessageChannel, MutableSet<Microservice>> = mutableMapOf()
+    private val subscribers: MutableMap<MessageChannel, MutableSet<Microservice>> = mutableMapOf()
+    private val msDatabase: MutableMap<Database, MutableSet<Microservice>> = mutableMapOf()
+    private val operations: MutableMap<Operation, MutableSet<Microservice>> = mutableMapOf()
+    private val results: MutableMap<Visitable, Measure> = mutableMapOf()
 
-    override fun getResults(): Map<Visitable, Measure> = counters
+
+    override fun getResults(): Map<Visitable, Measure> {
+        val allMicroservices = (publishers.values.flatten() + subscribers.values.flatten() + msDatabase.values.flatten() + operations.values.flatten())
+
+
+        allMicroservices.forEach { visitable ->
+            val microservice = visitable
+            val operationCount = microservice.getConsumedOperations().size
+            val producerCount = countProducers(microservice)
+            val databaseCount = microservice.getDatabases().size
+
+            val totalCount = operationCount + producerCount + databaseCount
+
+            this.results[microservice] = Count(
+                value = totalCount,
+                type = INT_TYPE_NAME,
+                unit = UNIT_DEPENDENCIES
+            )
+        }
+
+        return this.results
+    }
 
     override fun visit(microservice: Microservice) {
-        val count = microservice.getConsumedOperations().size +
-                microservice.getSubscribedChannels().size +
-                microservice.getDatabases().size
+        microservice.getPublishChannels().forEach { pubChannel ->
+            if (pubChannel !in this.publishers) {
+                this.publishers[pubChannel] = mutableSetOf()
+            }
 
-        counters[microservice] = Count(
-            value = count,
-            type = INT_TYPE_NAME, unit = UNIT_DEPENDENCIES
-        )
+            this.publishers[pubChannel]?.add(microservice)
+        }
+
+        microservice.getSubscribedChannels().forEach { subChannel ->
+            if (subChannel !in this.subscribers) {
+                this.subscribers[subChannel] = mutableSetOf()
+            }
+
+            this.subscribers[subChannel]?.add(microservice)
+        }
+
+        microservice.getDatabases().forEach { database ->
+            if (database !in this.msDatabase) {
+                this.msDatabase[database] = mutableSetOf()
+            }
+
+            this.msDatabase[database]?.add(microservice)
+        }
+
+        microservice.getConsumedOperations().forEach { operation ->
+            if (operation !in this.operations) {
+                this.operations[operation] = mutableSetOf()
+            }
+
+            this.operations[operation]?.add(microservice)
+        }
+
+        // Initialize the Count object for the microservice with a value of 0 to avoid null pointer exceptions
+        this.results[microservice] = Count(value = 0, type = INT_TYPE_NAME, unit = UNIT_DEPENDENCIES)
     }
+
+    private fun countProducers(microservice: Microservice): Int {
+        return microservice.getSubscribedChannels().sumOf { channel ->
+            publishers[channel]?.size ?: 0
+        }
+    }
+
 }
--- app-analyses/src/test/kotlin/com/usvision/analyses/analyzer/NumberofDependenciesTest.kt
+++ app-analyses/src/test/kotlin/com/usvision/analyses/analyzer/NumberofDependenciesTest.kt
@@ -1,5 +1,6 @@
 package com.usvision.analyses.analyzer
 
+import com.usvision.model.domain.CompanySystem
 import com.usvision.model.domain.Microservice
 import com.usvision.model.domain.MessageChannel
 import com.usvision.model.domain.databases.PostgreSQL
@@ -7,9 +8,8 @@ import com.usvision.model.domain.operations.RestEndpoint
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
-internal class NumberOfDependenciesTest {
+class NumberOfDependenciesTest {
     private lateinit var underTest: NumberOfDependencies
 
     @BeforeTest
@@ -19,83 +19,125 @@ internal class NumberOfDependenciesTest {
 
     @Test
     fun `it gets 0 from a microservice with no dependencies`() {
-        // given
-        val ms = Microservice(name = "noop")
+        val sys = CompanySystem(name = "test")
+        val ms = Microservice(name = "microservice")
+        sys.addSubsystem(ms)
 
-        // when
-        ms.accept(underTest)
+        sys.accept(underTest)
         val results = underTest.getResults()
 
-        // then
-        assertTrue { ms in results }
-        assertEquals(results[ms]!!.value, 0)
+        assertEquals(0, results[ms]?.value)
     }
 
     @Test
     fun `it gets 1 from a microservice with a single operation dependency`() {
-        // given
-        val ms = Microservice(name = "noop")
-        val op = RestEndpoint("GET", "/hello")
+        val sys = CompanySystem(name = "test")
+        val ms = Microservice(name = "microservice")
+        val op = RestEndpoint(httpVerb = "GET", path = "/data", "test")
         ms.consumeOperation(op)
+        sys.addSubsystem(ms)
 
-        // when
-        ms.accept(underTest)
+        sys.accept(underTest)
         val results = underTest.getResults()
 
-        // then
-        assertTrue { ms in results }
-        assertEquals(results[ms]!!.value, 1)
+        assertEquals(1, results[ms]?.value)
     }
 
     @Test
-    fun `it gets 1 from a microservice with a single channel dependency`() {
-        // given
-        val ms = Microservice(name = "noop")
-        val channel = MessageChannel(name = "topic", id = "1234-abcd")
+    fun `it gets 1 from a microservice with a single channel dependency and this channel with only one publisher`() {
+        val sys = CompanySystem(name = "test")
+        val ms = Microservice(name = "microservice")
+        val pub1 = Microservice(name = "publisher1")
+        val channel = MessageChannel(name = "channel", id = "1234-abcd")
         ms.addSubscribedChannel(channel)
+        pub1.addPublishChannel(channel)
+        sys.addSubsystem(ms)
+        sys.addSubsystem(pub1)
 
-        // when
-        ms.accept(underTest)
+        sys.accept(underTest)
         val results = underTest.getResults()
 
-        // then
-        assertTrue { ms in results }
-        assertEquals(results[ms]!!.value, 1)
+        assertEquals(1, results[ms]?.value)
+    }
+
+    @Test
+    fun `it gets 2 from a microservice with a single channel dependency and this channel with two publishers`() {
+        val sys = CompanySystem(name = "test")
+        val ms = Microservice(name = "microservice")
+        val pub1 = Microservice(name = "publisher1")
+        val pub2 = Microservice(name = "publisher2")
+        val channel = MessageChannel(name = "channel", id = "1234-abcd")
+        ms.addSubscribedChannel(channel)
+        pub1.addPublishChannel(channel)
+        pub2.addPublishChannel(channel)
+        sys.addSubsystem(ms)
+        sys.addSubsystem(pub1)
+        sys.addSubsystem(pub2)
+
+        sys.accept(underTest)
+        val results = underTest.getResults()
+
+        assertEquals(2, results[ms]?.value)
     }
 
     @Test
     fun `it gets 1 from a microservice with a single database dependency`() {
-        // given
-        val ms = Microservice(name = "noop")
-        val db = PostgreSQL(id = "db")
+        val sys = CompanySystem(name = "test")
+        val ms = Microservice(name = "microservice")
+        val db = PostgreSQL(id = "unique")
         ms.addDatabaseConnection(db)
+        sys.addSubsystem(ms)
 
-        // when
-        ms.accept(underTest)
+        sys.accept(underTest)
         val results = underTest.getResults()
 
-        // then
-        assertTrue { ms in results }
-        assertEquals(results[ms]!!.value, 1)
+        assertEquals(1, results[ms]?.value)
     }
 
     @Test
     fun `it gets 3 from a microservice with one of each type of dependency`() {
-        // given
-        val ms = Microservice(name = "noop")
-        val op = RestEndpoint("GET", "/hello")
+        val sys = CompanySystem(name = "test")
+        val ms = Microservice(name = "microservice")
+        val op = RestEndpoint(httpVerb = "GET", path = "/data", "test")
+        val pub = Microservice(name = "publisher")
+        val channel = MessageChannel(name = "channel", id = "1234-abcd")
+        val db = PostgreSQL(id = "unique")
         ms.consumeOperation(op)
-        val channel = MessageChannel(name = "topic", id = "1234-abcd")
         ms.addSubscribedChannel(channel)
-        val db = PostgreSQL(id = "db")
+        pub.addPublishChannel(channel)
         ms.addDatabaseConnection(db)
 
-        // when
-        ms.accept(underTest)
+        sys.addSubsystem(ms)
+        sys.addSubsystem(pub)
+
+        sys.accept(underTest)
         val results = underTest.getResults()
 
-        // then
-        assertTrue { ms in results }
-        assertEquals(results[ms]!!.value, 3)
+        assertEquals(3, results[ms]?.value)
+    }
+
+    @Test
+    fun `it gets 4 from a microservice with one of each type of dependency, and one channel with two publishers`() {
+        val sys = CompanySystem(name = "test")
+        val ms = Microservice(name = "microservice")
+        val op = RestEndpoint(httpVerb = "GET", path = "/data", "test")
+        val pub1 = Microservice(name = "publisher1")
+        val pub2 = Microservice(name = "publisher2")
+        val channel = MessageChannel(name = "channel", id = "1234-abcd")
+        val db = PostgreSQL(id = "unique")
+        ms.consumeOperation(op)
+        ms.addSubscribedChannel(channel)
+        pub1.addPublishChannel(channel)
+        pub2.addPublishChannel(channel)
+        ms.addDatabaseConnection(db)
+
+        sys.addSubsystem(ms)
+        sys.addSubsystem(pub1)
+        sys.addSubsystem(pub2)
+
+        sys.accept(underTest)
+        val results = underTest.getResults()
+
+        assertEquals(4, results[ms]?.value)
     }
 }
