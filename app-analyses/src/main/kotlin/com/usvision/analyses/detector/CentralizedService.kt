e770804a6b0bbcd75266331fabcb6b62a900e8b8
marcelo-vidu <marcelovidu@gmail.com>
2024-06-26 09:24:18 -0300

    Change detection method to check a threshold instead of the average of dependents.

--- app-analyses/src/main/kotlin/com/usvision/analyses/detector/CentralizedService.kt
+++ app-analyses/src/main/kotlin/com/usvision/analyses/detector/CentralizedService.kt
@@ -8,6 +8,10 @@ import com.usvision.model.visitor.Visitable
 class CentralizedService(
     private val numberOfDependents: NumberOfDependents
 ) : Detector() {
+    companion object {
+        const val DEPENDENT_THRESHOLD: Int = 3
+    }
+
     private lateinit var dependentResults: Map<Visitable, Measure>
     private var instances: MutableSet<CentralizedServiceInstance> = mutableSetOf()
 
@@ -16,12 +20,10 @@ class CentralizedService(
     }
 
     override fun combineMetric() {
-        val average = dependentResults.values.map { it.value as Int }.average()
-        dependentResults.forEach { (microservice, measure) ->
-            if (measure.value as Int > average) {
-                this.instances.add(CentralizedServiceInstance(microservice as Microservice, measure.value as Int))
-            }
-        }
+        instances = dependentResults
+            .filter { (_, measure) -> measure.value as Int > DEPENDENT_THRESHOLD }
+            .map { (ms, measure) -> CentralizedServiceInstance(ms as Microservice, measure.value as Int) }
+            .toMutableSet()
     }
 
     override fun getInstances(): Set<ArchitectureInsight> = instances
