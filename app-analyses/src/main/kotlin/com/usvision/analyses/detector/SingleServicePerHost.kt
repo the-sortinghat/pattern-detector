6c272602ff63a631df690dd5102e8cdbd702cd47
João Daniel <jotaf.daniel@gmail.com>
2024-06-27 21:27:45 +0200

    fix SingleServicePerHost cohesive threashold

--- app-analyses/src/main/kotlin/com/usvision/analyses/detector/SingleServicePerHost.kt
+++ app-analyses/src/main/kotlin/com/usvision/analyses/detector/SingleServicePerHost.kt
@@ -1,24 +1,28 @@
 package com.usvision.analyses.detector
 
-import com.usvision.analyses.analyzer.Measure
-import com.usvision.analyses.analyzer.MicroservicesOfModule
-import com.usvision.analyses.analyzer.NumberOfMicroservicesInAHost
-import com.usvision.analyses.analyzer.Relationship
+import com.usvision.analyses.analyzer.*
 import com.usvision.model.domain.Microservice
 import com.usvision.model.domain.Module
 import com.usvision.model.visitor.Visitable
 
 class SingleServicePerHost(
     private val coHostedMsv: MicroservicesOfModule,
-    private val nMsvPerHost: NumberOfMicroservicesInAHost
+    private val nMsvPerHost: NumberOfMicroservicesInAHost,
+    private val nops: NumberOfExposedOperations,
 ) : Detector() {
+    companion object {
+        const val MAX_COHESIVE_THRESHOLD: Int = 9
+    }
+
     private lateinit var instances: Set<SingleServicePerHostInstance>
     private lateinit var nMsvcInHost: Map<Visitable, Measure>
     private lateinit var cohostedServices: Map<Visitable, Set<Relationship>>
+    private lateinit var nopsPerService: Map<Visitable, Measure>
 
     override fun collectMetrics() {
         nMsvcInHost = nMsvPerHost.getResults()
         cohostedServices = coHostedMsv.getResults()
+        nopsPerService = nops.getResults()
     }
 
     override fun combineMetric() {
@@ -31,6 +35,7 @@ class SingleServicePerHost(
                     cohostedServices[module]!!.first().with as Microservice
                 )
             }
+            .filter { (nopsPerService[it.second]!!.value as Int) <= MAX_COHESIVE_THRESHOLD }
             .map { SingleServicePerHostInstance.of(it) }
             .toSet()
     }
--- app-analyses/src/test/kotlin/com/usvision/analyses/detector/SingleServicePerHostTest.kt
+++ app-analyses/src/test/kotlin/com/usvision/analyses/detector/SingleServicePerHostTest.kt
@@ -1,18 +1,12 @@
 package com.usvision.analyses.detector
 
-import com.usvision.analyses.analyzer.Count
-import com.usvision.analyses.analyzer.MicroservicesOfModule
-import com.usvision.analyses.analyzer.NumberOfMicroservicesInAHost
-import com.usvision.analyses.analyzer.Relationship
+import com.usvision.analyses.analyzer.*
 import com.usvision.model.domain.Microservice
 import com.usvision.model.domain.Module
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import kotlin.test.BeforeTest
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertIs
+import kotlin.test.*
 
 internal class SingleServicePerHostTest {
     private lateinit var underTest: SingleServicePerHost
@@ -23,12 +17,16 @@ internal class SingleServicePerHostTest {
     @MockK
     private lateinit var mockNumberOfMicroservicesInAHost: NumberOfMicroservicesInAHost
 
+    @MockK
+    private lateinit var mockNops: NumberOfExposedOperations
+
     @BeforeTest
     fun `create clean, new instance of SingleServicePerHost`() {
         MockKAnnotations.init(this)
         underTest = SingleServicePerHost(
             coHostedMsv = mockMicroservicesOfModule,
-            nMsvPerHost = mockNumberOfMicroservicesInAHost
+            nMsvPerHost = mockNumberOfMicroservicesInAHost,
+            nops = mockNops
         )
     }
 
@@ -43,6 +41,13 @@ internal class SingleServicePerHostTest {
         every { mockNumberOfMicroservicesInAHost.getResults() } returns mapOf(
             module to Count(value = 1, type = "Int", unit = "microservices")
         )
+        every { mockNops.getResults() } returns mapOf(
+            microservice to Count(
+                value = SingleServicePerHost.MAX_COHESIVE_THRESHOLD - 1,
+                type = "Int",
+                unit = "operations"
+            )
+        )
 
         // when
         underTest.run()
@@ -51,6 +56,33 @@ internal class SingleServicePerHostTest {
         // then
         assertEquals(results.size, 1)
         assertIs<SingleServicePerHostInstance>(results.first())
+    }
+
+    @Test
+    fun `it does not detect when given a module with a non-cohesive single microservice`() {
+        // given
+        val module = Module.createWithId()
+        val microservice = Microservice(name = "foo")
+        every { mockMicroservicesOfModule.getResults() } returns mapOf(
+            module to setOf(Relationship(with = microservice))
+        )
+        every { mockNumberOfMicroservicesInAHost.getResults() } returns mapOf(
+            module to Count(value = 1, type = "Int", unit = "microservices")
+        )
+        every { mockNops.getResults() } returns mapOf(
+            microservice to Count(
+                value = SingleServicePerHost.MAX_COHESIVE_THRESHOLD + 1,
+                type = "Int",
+                unit = "operations"
+            )
+        )
+
+        // when
+        underTest.run()
+        val results = underTest.getInstances()
+
+        // then
+        assertTrue { results.isEmpty() }
     }
 
     @Test
@@ -64,6 +96,18 @@ internal class SingleServicePerHostTest {
         )
         every { mockNumberOfMicroservicesInAHost.getResults() } returns mapOf(
             module to Count(value = 2, type = "Int", unit = "microservices")
+        )
+        every { mockNops.getResults() } returns mapOf(
+            foo to Count(
+                value = SingleServicePerHost.MAX_COHESIVE_THRESHOLD - 1,
+                type = "Int",
+                unit = "operations"
+            ),
+            baz to Count(
+                value = SingleServicePerHost.MAX_COHESIVE_THRESHOLD - 1,
+                type = "Int",
+                unit = "operations"
+            )
         )
 
         // when
