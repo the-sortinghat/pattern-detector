6ffe9642f1799c8e4b83a7c511a7581af23042e4
marcelo-vidu <marcelovidu@gmail.com>
2024-06-27 19:51:54 -0300

    Fix detection process when a microservice is part of more than one smell instance.

--- app-analyses/src/main/kotlin/com/usvision/analyses/detector/CyclicDependency.kt
+++ app-analyses/src/main/kotlin/com/usvision/analyses/detector/CyclicDependency.kt
@@ -64,22 +64,29 @@ class CyclicDependency(
 
         visited.add(startingFrom)
         deps[startingFrom]?.forEach { neighbor ->
-            parentOf[neighbor] = startingFrom
-            val isFound = findOneCycle(neighbor, visited, parentOf)
-            if (isFound) return true
+            if (!parentOf.containsKey(neighbor)) {
+                parentOf[neighbor] = startingFrom
+                val isFound = findOneCycle(neighbor, visited, parentOf)
+                if (isFound) return true
+            }
         }
 
+        visited.remove(startingFrom)
         return false
     }
 
     private fun findCycles(): Map<Microservice, Map<Microservice, Microservice>> {
         val cycles = mutableMapOf<Microservice, Map<Microservice, Microservice>>()
-        deps.keys.forEach {
+        deps.keys.forEach { microservice ->
             val parents = mutableMapOf<Microservice, Microservice>()
-            val isFound = findOneCycle(startingFrom = it, mutableSetOf(), parents)
-            if (isFound) cycles[it] = parents
+            val visited = mutableSetOf<Microservice>()
+            val isFound = findOneCycle(startingFrom = microservice, visited = visited, parentOf = parents)
+            if (isFound) {
+                cycles[microservice] = parents
+            }
         }
 
         return cycles
     }
+
 }
