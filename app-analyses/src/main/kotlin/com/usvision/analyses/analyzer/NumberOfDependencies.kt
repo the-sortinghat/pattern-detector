0e4af1e1e941d943a91b6400a15c7eb2cb7aae12
marcelo-vidu <marcelovidu@gmail.com>
2024-06-28 21:02:58 -0300

    Fix number of sync dependencies when more than one operation is consumed from the same microservice.

--- app-analyses/src/main/kotlin/com/usvision/analyses/analyzer/NumberOfDependencies.kt
+++ app-analyses/src/main/kotlin/com/usvision/analyses/analyzer/NumberOfDependencies.kt
@@ -12,24 +12,21 @@ class NumberOfDependencies : Measurer() {
 
     private val publishers: MutableMap<MessageChannel, MutableSet<Microservice>> = mutableMapOf()
     private val subscribers: MutableMap<MessageChannel, MutableSet<Microservice>> = mutableMapOf()
+    private val producers = mutableMapOf<Operation, MutableSet<Microservice>>()
+    private val consumers = mutableMapOf<Operation, MutableSet<Microservice>>()
     private val msDatabase: MutableMap<Database, MutableSet<Microservice>> = mutableMapOf()
-    private val operations: MutableMap<Operation, MutableSet<Microservice>> = mutableMapOf()
     private val results: MutableMap<Visitable, Measure> = mutableMapOf()
 
-
     override fun getResults(): Map<Visitable, Measure> {
-        val allMicroservices = (publishers.values.flatten() + subscribers.values.flatten() + msDatabase.values.flatten() + operations.values.flatten())
-
+        val allMicroservices = (publishers.values.flatten() + subscribers.values.flatten() + msDatabase.values.flatten() + consumers.values.flatten()).toSet()
 
         allMicroservices.forEach { visitable ->
             val microservice = visitable
-            val operationMicroservices = microservice.getConsumedOperations().map { operation ->
-                operations[operation]?.firstOrNull()
-            }.filterNotNull().distinct().size
-            val producerCount = countProducers(microservice)
+            val consumersCount = countMsConsumedOps(microservice)
+            val subscribersCount = countSubscribers(microservice)
             val databaseCount = microservice.getDatabases().size
 
-            val totalCount = operationMicroservices + producerCount + databaseCount
+            val totalCount = consumersCount + subscribersCount + databaseCount
 
             this.results[microservice] = Count(
                 value = totalCount,
@@ -43,45 +40,38 @@ class NumberOfDependencies : Measurer() {
 
     override fun visit(microservice: Microservice) {
         microservice.getPublishChannels().forEach { pubChannel ->
-            if (pubChannel !in this.publishers) {
-                this.publishers[pubChannel] = mutableSetOf()
-            }
-
-            this.publishers[pubChannel]?.add(microservice)
+            publishers.computeIfAbsent(pubChannel) { mutableSetOf() }.add(microservice)
         }
 
         microservice.getSubscribedChannels().forEach { subChannel ->
-            if (subChannel !in this.subscribers) {
-                this.subscribers[subChannel] = mutableSetOf()
-            }
-
-            this.subscribers[subChannel]?.add(microservice)
+            subscribers.computeIfAbsent(subChannel) { mutableSetOf() }.add(microservice)
         }
 
         microservice.getDatabases().forEach { database ->
-            if (database !in this.msDatabase) {
-                this.msDatabase[database] = mutableSetOf()
-            }
+            msDatabase.computeIfAbsent(database) { mutableSetOf() }.add(microservice)
+        }
 
-            this.msDatabase[database]?.add(microservice)
+        microservice.getExposedOperations().forEach { operation ->
+            producers.computeIfAbsent(operation) { mutableSetOf() }.add(microservice)
         }
 
         microservice.getConsumedOperations().forEach { operation ->
-            if (operation !in this.operations) {
-                this.operations[operation] = mutableSetOf()
-            }
-
-            this.operations[operation]?.add(microservice)
+            consumers.computeIfAbsent(operation) { mutableSetOf() }.add(microservice)
         }
 
         // Initialize the Count object for the microservice with a value of 0 to avoid null pointer exceptions
         this.results[microservice] = Count(value = 0, type = INT_TYPE_NAME, unit = UNIT_DEPENDENCIES)
     }
 
-    private fun countProducers(microservice: Microservice): Int {
+    private fun countSubscribers(microservice: Microservice): Int {
         return microservice.getSubscribedChannels().sumOf { channel ->
             publishers[channel]?.size ?: 0
         }
     }
 
+    private fun countMsConsumedOps(microservice: Microservice): Int {
+        return microservice.getConsumedOperations().flatMap { operation ->
+            producers[operation] ?: emptySet()
+        }.distinct().size
+    }
 }
