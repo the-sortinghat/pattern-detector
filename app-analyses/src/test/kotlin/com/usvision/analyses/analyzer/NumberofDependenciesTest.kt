780b103b4bb2500808802f0f90500115705abc93
marcelo-vidu <marcelovidu@gmail.com>
2024-06-28 21:04:42 -0300

    Adjust test cases after NumberOfDependencies class fixes.

--- app-analyses/src/test/kotlin/com/usvision/analyses/analyzer/NumberofDependenciesTest.kt
+++ app-analyses/src/test/kotlin/com/usvision/analyses/analyzer/NumberofDependenciesTest.kt
@@ -32,52 +32,67 @@ class NumberOfDependenciesTest {
     @Test
     fun `it gets 1 from a microservice with a single operation dependency`() {
         val sys = CompanySystem(name = "test")
-        val ms = Microservice(name = "microservice")
+        val producer = Microservice(name = "producer")
+        val consumer = Microservice(name = "consumer")
         val op = RestEndpoint(httpVerb = "GET", path = "/data", "test")
-        ms.consumeOperation(op)
-        sys.addSubsystem(ms)
+
+        producer.exposeOperation(op)
+        consumer.consumeOperation(op)
+
+        sys.addSubsystem(producer)
+        sys.addSubsystem(consumer)
 
         sys.accept(underTest)
         val results = underTest.getResults()
 
-        assertEquals(1, results[ms]?.value)
+        assertEquals(1, results[consumer]?.value)
     }
 
     @Test
     fun `it gets 1 from a microservice with two operations dependency, but both from the same microservice`() {
         val sys = CompanySystem(name = "test")
-        val ms = Microservice(name = "microservice")
+        val producer = Microservice(name = "producer")
+        val consumer = Microservice(name = "consumer")
         val op1 = RestEndpoint(httpVerb = "GET", path = "/data1", "test1")
         val op2 = RestEndpoint(httpVerb = "GET", path = "/data2", "test2")
-        ms.consumeOperation(op1)
-        ms.consumeOperation(op2)
-        sys.addSubsystem(ms)
+
+        producer.exposeOperation(op1)
+        producer.exposeOperation(op2)
+        consumer.consumeOperation(op1)
+        consumer.consumeOperation(op2)
+        sys.addSubsystem(producer)
+        sys.addSubsystem(consumer)
 
         sys.accept(underTest)
         val results = underTest.getResults()
 
-        assertEquals(1, results[ms]?.value)
+        assertEquals(1, results[consumer]?.value)
     }
 
     @Test
     fun `it gets 1 from a microservice with two operations dependency and also 1 from a microservice with only one operation dependency`() {
         val sys = CompanySystem(name = "test")
-        val ms1 = Microservice(name = "microservice1")
-        val ms2 = Microservice(name = "microservice2")
+        val producer = Microservice(name = "producer")
+        val consumer1 = Microservice(name = "consumer1")
+        val consumer2 = Microservice(name = "consumer2")
         val op1 = RestEndpoint(httpVerb = "GET", path = "/data1", "test1")
         val op2 = RestEndpoint(httpVerb = "GET", path = "/data2", "test2")
         val op3 = RestEndpoint(httpVerb = "POST", path = "/data3", "test3")
-        ms1.consumeOperation(op1)
-        ms1.consumeOperation(op2)
-        ms2.consumeOperation(op3)
-        sys.addSubsystem(ms1)
-        sys.addSubsystem(ms2)
+        producer.exposeOperation(op1)
+        producer.exposeOperation(op2)
+        producer.exposeOperation(op3)
+        consumer1.consumeOperation(op1)
+        consumer1.consumeOperation(op2)
+        consumer2.consumeOperation(op3)
+        sys.addSubsystem(producer)
+        sys.addSubsystem(consumer1)
+        sys.addSubsystem(consumer2)
 
         sys.accept(underTest)
         val results = underTest.getResults()
 
-        assertEquals(1, results[ms1]?.value)
-        assertEquals(1, results[ms2]?.value)
+        assertEquals(1, results[consumer1]?.value)
+        assertEquals(1, results[consumer2]?.value)
     }
 
     @Test
@@ -134,47 +149,55 @@ class NumberOfDependenciesTest {
     @Test
     fun `it gets 3 from a microservice with one of each type of dependency`() {
         val sys = CompanySystem(name = "test")
-        val ms = Microservice(name = "microservice")
-        val op = RestEndpoint(httpVerb = "GET", path = "/data", "test")
+        val ms1 = Microservice(name = "exposeroperation")
+        val ms2 = Microservice(name = "consumeroperation")
         val pub = Microservice(name = "publisher")
+        val op = RestEndpoint(httpVerb = "GET", path = "/data", "test")
         val channel = MessageChannel(name = "channel", id = "1234-abcd")
         val db = PostgreSQL(id = "unique")
-        ms.consumeOperation(op)
-        ms.addSubscribedChannel(channel)
-        pub.addPublishChannel(channel)
-        ms.addDatabaseConnection(db)
 
-        sys.addSubsystem(ms)
+        sys.addSubsystem(ms1)
+        sys.addSubsystem(ms2)
         sys.addSubsystem(pub)
+
+        ms1.exposeOperation(op)
+        pub.addPublishChannel(channel)
+        ms2.consumeOperation(op)
+        ms2.addSubscribedChannel(channel)
+        ms2.addDatabaseConnection(db)
 
         sys.accept(underTest)
         val results = underTest.getResults()
 
-        assertEquals(3, results[ms]?.value)
+        assertEquals(3, results[ms2]?.value)
     }
 
     @Test
     fun `it gets 4 from a microservice with one of each type of dependency, and one channel with two publishers`() {
         val sys = CompanySystem(name = "test")
-        val ms = Microservice(name = "microservice")
-        val op = RestEndpoint(httpVerb = "GET", path = "/data", "test")
+        val ms1 = Microservice(name = "exposeroperation")
+        val ms2 = Microservice(name = "consumeroperation")
         val pub1 = Microservice(name = "publisher1")
         val pub2 = Microservice(name = "publisher2")
+        val op = RestEndpoint(httpVerb = "GET", path = "/data", "test")
         val channel = MessageChannel(name = "channel", id = "1234-abcd")
         val db = PostgreSQL(id = "unique")
-        ms.consumeOperation(op)
-        ms.addSubscribedChannel(channel)
+
+        ms1.exposeOperation(op)
         pub1.addPublishChannel(channel)
         pub2.addPublishChannel(channel)
-        ms.addDatabaseConnection(db)
+        ms2.consumeOperation(op)
+        ms2.addSubscribedChannel(channel)
+        ms2.addDatabaseConnection(db)
 
-        sys.addSubsystem(ms)
+        sys.addSubsystem(ms1)
+        sys.addSubsystem(ms2)
         sys.addSubsystem(pub1)
         sys.addSubsystem(pub2)
 
         sys.accept(underTest)
         val results = underTest.getResults()
 
-        assertEquals(4, results[ms]?.value)
+        assertEquals(4, results[ms2]?.value)
     }
 }
